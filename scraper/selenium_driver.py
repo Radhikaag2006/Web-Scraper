9b7b5b20ab2dbbefc9d5e7a1f7ae3c6cc47e77e9
Radhika Agrawal <radhikaagrawal1905@gmail.com>
2026-06-29 08:37:29 +0530

    Headless Mode

--- scraper/selenium_driver.py
+++ scraper/selenium_driver.py
@@ -2,8 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options 
 
 def get_driver():
+  options = Options()
 
-    options = Options()
-    options.add_argument("--start-maximized")
-    driver = webdriver.Chrome(options = options)
-    return driver
+  while True:
+    mode = input("Would you like to run it in headless mode? (y/n):").strip().lower()
+    if mode in ("y", "n"):
+      break
+    print("Please enter only 'y' or 'n'.")
+
+  headless = mode == "y"
+
+  if headless:
+    options.add_argument("--headless=new")
+
+  options.add_argument("--start-maximized")
+  driver = webdriver.Chrome(options=options)
+  return driver
